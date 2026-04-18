bdb09838d126a42c33053c0271f49b5f18ffd324
Denis Mixa <Denis.Mixa@frengp.cz>
2026-04-18 20:18:35 +0200

    Přidání hlavního program z domova

--- WinFormsApp1/FormHra.Designer.cs
+++ WinFormsApp1/FormHra.Designer.cs
@@ -28,19 +28,408 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pnlSibenice = new Panel();
+            lblSlovo = new Label();
+            lblZivoty = new Label();
+            pnlKlavesnice = new Panel();
+            btnM = new Button();
+            btnN = new Button();
+            btnB = new Button();
+            btnV = new Button();
+            btnC = new Button();
+            btnX = new Button();
+            btnZ = new Button();
+            btnP = new Button();
+            btnO = new Button();
+            btnI = new Button();
+            btnU = new Button();
+            btnY = new Button();
+            btnT = new Button();
+            btnR = new Button();
+            btnE = new Button();
+            btnW = new Button();
+            btnQ = new Button();
+            btnL = new Button();
+            btnK = new Button();
+            btnJ = new Button();
+            btnH = new Button();
+            btnG = new Button();
+            btnF = new Button();
+            btnD = new Button();
+            btnS = new Button();
+            btnA = new Button();
+            pnlKlavesnice.SuspendLayout();
             SuspendLayout();
+            // 
+            // pnlSibenice
+            // 
+            pnlSibenice.Location = new Point(24, 12);
+            pnlSibenice.Name = "pnlSibenice";
+            pnlSibenice.Size = new Size(246, 287);
+            pnlSibenice.TabIndex = 0;
+            pnlSibenice.Paint += pnlSibenice_Paint;
+            // 
+            // lblSlovo
+            // 
+            lblSlovo.AutoSize = true;
+            lblSlovo.Font = new Font("Segoe UI", 24F);
+            lblSlovo.Location = new Point(460, 49);
+            lblSlovo.Name = "lblSlovo";
+            lblSlovo.Size = new Size(105, 45);
+            lblSlovo.TabIndex = 1;
+            lblSlovo.Text = "label1";
+            // 
+            // lblZivoty
+            // 
+            lblZivoty.AutoSize = true;
+            lblZivoty.Font = new Font("Segoe UI", 15F);
+            lblZivoty.Location = new Point(712, 62);
+            lblZivoty.Name = "lblZivoty";
+            lblZivoty.Size = new Size(65, 28);
+            lblZivoty.TabIndex = 2;
+            lblZivoty.Text = "label1";
+            // 
+            // pnlKlavesnice
+            // 
+            pnlKlavesnice.Controls.Add(btnM);
+            pnlKlavesnice.Controls.Add(btnN);
+            pnlKlavesnice.Controls.Add(btnB);
+            pnlKlavesnice.Controls.Add(btnV);
+            pnlKlavesnice.Controls.Add(btnC);
+            pnlKlavesnice.Controls.Add(btnX);
+            pnlKlavesnice.Controls.Add(btnZ);
+            pnlKlavesnice.Controls.Add(btnP);
+            pnlKlavesnice.Controls.Add(btnO);
+            pnlKlavesnice.Controls.Add(btnI);
+            pnlKlavesnice.Controls.Add(btnU);
+            pnlKlavesnice.Controls.Add(btnY);
+            pnlKlavesnice.Controls.Add(btnT);
+            pnlKlavesnice.Controls.Add(btnR);
+            pnlKlavesnice.Controls.Add(btnE);
+            pnlKlavesnice.Controls.Add(btnW);
+            pnlKlavesnice.Controls.Add(btnQ);
+            pnlKlavesnice.Controls.Add(btnL);
+            pnlKlavesnice.Controls.Add(btnK);
+            pnlKlavesnice.Controls.Add(btnJ);
+            pnlKlavesnice.Controls.Add(btnH);
+            pnlKlavesnice.Controls.Add(btnG);
+            pnlKlavesnice.Controls.Add(btnF);
+            pnlKlavesnice.Controls.Add(btnD);
+            pnlKlavesnice.Controls.Add(btnS);
+            pnlKlavesnice.Controls.Add(btnA);
+            pnlKlavesnice.Location = new Point(24, 321);
+            pnlKlavesnice.Name = "pnlKlavesnice";
+            pnlKlavesnice.Size = new Size(922, 202);
+            pnlKlavesnice.TabIndex = 3;
+            // 
+            // btnM
+            // 
+            btnM.Location = new Point(294, 114);
+            btnM.Name = "btnM";
+            btnM.Size = new Size(40, 40);
+            btnM.TabIndex = 25;
+            btnM.Text = "M";
+            btnM.UseVisualStyleBackColor = true;
+            btnM.Click += Pismeno_Click;
+            // 
+            // btnN
+            // 
+            btnN.Location = new Point(248, 114);
+            btnN.Name = "btnN";
+            btnN.Size = new Size(40, 40);
+            btnN.TabIndex = 24;
+            btnN.Text = "N";
+            btnN.UseVisualStyleBackColor = true;
+            btnN.Click += Pismeno_Click;
+            // 
+            // btnB
+            // 
+            btnB.Location = new Point(202, 114);
+            btnB.Name = "btnB";
+            btnB.Size = new Size(40, 40);
+            btnB.TabIndex = 23;
+            btnB.Text = "B";
+            btnB.UseVisualStyleBackColor = true;
+            btnB.Click += Pismeno_Click;
+            // 
+            // btnV
+            // 
+            btnV.Location = new Point(156, 114);
+            btnV.Name = "btnV";
+            btnV.Size = new Size(40, 40);
+            btnV.TabIndex = 22;
+            btnV.Text = "V";
+            btnV.UseVisualStyleBackColor = true;
+            btnV.Click += Pismeno_Click;
+            // 
+            // btnC
+            // 
+            btnC.Location = new Point(110, 114);
+            btnC.Name = "btnC";
+            btnC.Size = new Size(40, 40);
+            btnC.TabIndex = 21;
+            btnC.Text = "C";
+            btnC.UseVisualStyleBackColor = true;
+            btnC.Click += Pismeno_Click;
+            // 
+            // btnX
+            // 
+            btnX.Location = new Point(64, 114);
+            btnX.Name = "btnX";
+            btnX.Size = new Size(40, 40);
+            btnX.TabIndex = 20;
+            btnX.Text = "X";
+            btnX.UseVisualStyleBackColor = true;
+            btnX.Click += Pismeno_Click;
+            // 
+            // btnZ
+            // 
+            btnZ.Location = new Point(18, 114);
+            btnZ.Name = "btnZ";
+            btnZ.Size = new Size(40, 40);
+            btnZ.TabIndex = 19;
+            btnZ.Text = "Z";
+            btnZ.UseVisualStyleBackColor = true;
+            btnZ.Click += Pismeno_Click;
+            // 
+            // btnP
+            // 
+            btnP.Location = new Point(432, 22);
+            btnP.Name = "btnP";
+            btnP.Size = new Size(40, 40);
+            btnP.TabIndex = 18;
+            btnP.Text = "P";
+            btnP.UseVisualStyleBackColor = true;
+            btnP.Click += Pismeno_Click;
+            // 
+            // btnO
+            // 
+            btnO.Location = new Point(386, 22);
+            btnO.Name = "btnO";
+            btnO.Size = new Size(40, 40);
+            btnO.TabIndex = 17;
+            btnO.Text = "O";
+            btnO.UseVisualStyleBackColor = true;
+            btnO.Click += Pismeno_Click;
+            // 
+            // btnI
+            // 
+            btnI.Location = new Point(340, 22);
+            btnI.Name = "btnI";
+            btnI.Size = new Size(40, 40);
+            btnI.TabIndex = 16;
+            btnI.Text = "I";
+            btnI.UseVisualStyleBackColor = true;
+            btnI.Click += Pismeno_Click;
+            // 
+            // btnU
+            // 
+            btnU.Location = new Point(294, 22);
+            btnU.Name = "btnU";
+            btnU.Size = new Size(40, 40);
+            btnU.TabIndex = 15;
+            btnU.Text = "U";
+            btnU.UseVisualStyleBackColor = true;
+            btnU.Click += Pismeno_Click;
+            // 
+            // btnY
+            // 
+            btnY.Location = new Point(248, 22);
+            btnY.Name = "btnY";
+            btnY.Size = new Size(40, 40);
+            btnY.TabIndex = 14;
+            btnY.Text = "Y";
+            btnY.UseVisualStyleBackColor = true;
+            btnY.Click += Pismeno_Click;
+            // 
+            // btnT
+            // 
+            btnT.Location = new Point(202, 22);
+            btnT.Name = "btnT";
+            btnT.Size = new Size(40, 40);
+            btnT.TabIndex = 13;
+            btnT.Text = "T";
+            btnT.UseVisualStyleBackColor = true;
+            btnT.Click += Pismeno_Click;
+            // 
+            // btnR
+            // 
+            btnR.Location = new Point(156, 22);
+            btnR.Name = "btnR";
+            btnR.Size = new Size(40, 40);
+            btnR.TabIndex = 12;
+            btnR.Text = "R";
+            btnR.UseVisualStyleBackColor = true;
+            btnR.Click += Pismeno_Click;
+            // 
+            // btnE
+            // 
+            btnE.Location = new Point(110, 22);
+            btnE.Name = "btnE";
+            btnE.Size = new Size(40, 40);
+            btnE.TabIndex = 11;
+            btnE.Text = "E";
+            btnE.UseVisualStyleBackColor = true;
+            btnE.Click += Pismeno_Click;
+            // 
+            // btnW
+            // 
+            btnW.Location = new Point(64, 22);
+            btnW.Name = "btnW";
+            btnW.Size = new Size(40, 40);
+            btnW.TabIndex = 10;
+            btnW.Text = "W";
+            btnW.UseVisualStyleBackColor = true;
+            btnW.Click += Pismeno_Click;
+            // 
+            // btnQ
+            // 
+            btnQ.Location = new Point(18, 22);
+            btnQ.Name = "btnQ";
+            btnQ.Size = new Size(40, 40);
+            btnQ.TabIndex = 9;
+            btnQ.Text = "Q";
+            btnQ.UseVisualStyleBackColor = true;
+            btnQ.Click += Pismeno_Click;
+            // 
+            // btnL
+            // 
+            btnL.Location = new Point(386, 68);
+            btnL.Name = "btnL";
+            btnL.Size = new Size(40, 40);
+            btnL.TabIndex = 8;
+            btnL.Text = "L";
+            btnL.UseVisualStyleBackColor = true;
+            btnL.Click += Pismeno_Click;
+            // 
+            // btnK
+            // 
+            btnK.Location = new Point(340, 68);
+            btnK.Name = "btnK";
+            btnK.Size = new Size(40, 40);
+            btnK.TabIndex = 7;
+            btnK.Text = "K";
+            btnK.UseVisualStyleBackColor = true;
+            btnK.Click += Pismeno_Click;
+            // 
+            // btnJ
+            // 
+            btnJ.Location = new Point(294, 68);
+            btnJ.Name = "btnJ";
+            btnJ.Size = new Size(40, 40);
+            btnJ.TabIndex = 6;
+            btnJ.Text = "J";
+            btnJ.UseVisualStyleBackColor = true;
+            btnJ.Click += Pismeno_Click;
+            // 
+            // btnH
+            // 
+            btnH.Location = new Point(248, 68);
+            btnH.Name = "btnH";
+            btnH.Size = new Size(40, 40);
+            btnH.TabIndex = 5;
+            btnH.Text = "H";
+            btnH.UseVisualStyleBackColor = true;
+            btnH.Click += Pismeno_Click;
+            // 
+            // btnG
+            // 
+            btnG.Location = new Point(202, 68);
+            btnG.Name = "btnG";
+            btnG.Size = new Size(40, 40);
+            btnG.TabIndex = 4;
+            btnG.Text = "G";
+            btnG.UseVisualStyleBackColor = true;
+            btnG.Click += Pismeno_Click;
+            // 
+            // btnF
+            // 
+            btnF.Location = new Point(156, 68);
+            btnF.Name = "btnF";
+            btnF.Size = new Size(40, 40);
+            btnF.TabIndex = 3;
+            btnF.Text = "F";
+            btnF.UseVisualStyleBackColor = true;
+            btnF.Click += Pismeno_Click;
+            // 
+            // btnD
+            // 
+            btnD.Location = new Point(110, 68);
+            btnD.Name = "btnD";
+            btnD.Size = new Size(40, 40);
+            btnD.TabIndex = 2;
+            btnD.Text = "D";
+            btnD.UseVisualStyleBackColor = true;
+            btnD.Click += Pismeno_Click;
+            // 
+            // btnS
+            // 
+            btnS.Location = new Point(64, 68);
+            btnS.Name = "btnS";
+            btnS.Size = new Size(40, 40);
+            btnS.TabIndex = 1;
+            btnS.Text = "S";
+            btnS.UseVisualStyleBackColor = true;
+            btnS.Click += Pismeno_Click;
+            // 
+            // btnA
+            // 
+            btnA.Location = new Point(18, 68);
+            btnA.Name = "btnA";
+            btnA.Size = new Size(40, 40);
+            btnA.TabIndex = 0;
+            btnA.Text = "A";
+            btnA.UseVisualStyleBackColor = true;
+            btnA.Click += Pismeno_Click;
             // 
             // FormHra
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(973, 546);
+            Controls.Add(pnlKlavesnice);
+            Controls.Add(lblZivoty);
+            Controls.Add(lblSlovo);
+            Controls.Add(pnlSibenice);
             Name = "FormHra";
             Text = "FormHra";
             Load += FormHra_Load;
+            pnlKlavesnice.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel pnlSibenice;
+        private Label lblSlovo;
+        private Label lblZivoty;
+        private Panel pnlKlavesnice;
+        private Button btnJ;
+        private Button btnH;
+        private Button btnG;
+        private Button btnF;
+        private Button btnD;
+        private Button btnS;
+        private Button btnA;
+        private Button btnN;
+        private Button btnB;
+        private Button btnV;
+        private Button btnC;
+        private Button btnX;
+        private Button btnZ;
+        private Button btnP;
+        private Button btnO;
+        private Button btnI;
+        private Button btnU;
+        private Button btnY;
+        private Button btnT;
+        private Button btnR;
+        private Button btnE;
+        private Button btnW;
+        private Button btnQ;
+        private Button btnL;
+        private Button btnK;
+        private Button btnM;
     }
 }
--- WinFormsApp1/FormHra.cs
+++ WinFormsApp1/FormHra.cs
@@ -12,6 +12,11 @@ namespace WinFormsApp1
 {
     public partial class FormHra : Form
     {
+
+        string slovo = "skola";
+        char[] hadaneSlovo;
+        int zivoty = 10;
+
         public FormHra()
         {
             InitializeComponent();
@@ -19,7 +24,80 @@ namespace WinFormsApp1
 
         private void FormHra_Load(object sender, EventArgs e)
         {
+            hadaneSlovo = new char[slovo.Length];
+
+            for (int i = 0; i < slovo.Length; i++)
+                hadaneSlovo[i] = '_';
+
+            AktualizujSlovo();
+        }
+
+        private void pnlSibenice_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black, 2);
+
+            int chyby = 10 - zivoty;
 
         }
+
+
+        void AktualizujSlovo()
+        {
+            lblSlovo.Text = "";
+
+            for (int i = 0; i < hadaneSlovo.Length; i++)
+                lblSlovo.Text += hadaneSlovo[i] + " ";
+
+            lblZivoty.Text = "Životy: " + zivoty;
+        }
+
+        private void Pismeno_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            b.Enabled = false;
+
+            char pismeno = char.ToLower(b.Text[0]);
+            bool trefa = false;
+
+            for (int i = 0; i < slovo.Length; i++)
+            {
+                if (slovo[i] == pismeno)
+                {
+                    hadaneSlovo[i] = pismeno;
+                    trefa = true;
+                }
+            }
+
+            if (!trefa)
+                zivoty--;
+
+            AktualizujSlovo();
+            pnlSibenice.Invalidate();
+
+
+            bool vyhra = true;
+
+            for (int i = 0; i < hadaneSlovo.Length; i++)
+            {
+                if (hadaneSlovo[i] == '_')
+                    vyhra = false;
+            }
+
+            if (vyhra)
+            {
+                MessageBox.Show("Vyhrál jsi!");
+            }
+
+
+            if (zivoty <= 0)
+            {
+                MessageBox.Show("Prohrál jsi!");
+            }
+
+        }
+
+
     }
 }
